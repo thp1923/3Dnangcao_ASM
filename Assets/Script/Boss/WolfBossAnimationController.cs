b63df5bafa911240e45439297ec580752bb1ffb9
nvmq1011 <quannvmps38853@gmail.com>
2025-02-16 11:33:32 +0700

    1

--- Assets/Script/Boss/WolfBossAnimationController.cs
+++ Assets/Script/Boss/WolfBossAnimationController.cs
@@ -7,25 +7,21 @@ using UnityEngine.AI;
 public class WolfBossAnimationController : MonoBehaviour
 {
     [Header("Public")]
-    public Animator animator;
-    public float distanceMove;
+    public Transform point;
 
     [Header("No Public")]
-    Transform player;
     NavMeshAgent agent;
-    float distance;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
         agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        distance = Vector3.Distance(player.position, gameObject.transform.position);
+        agent.SetDestination(point.position);
     }
 
     
