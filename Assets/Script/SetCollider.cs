1ed7738988fc49b8edbdc4b8c90cbf6a921f6b66
Tran Hoang Phuc <conanthy123@gmail.com>
2025-03-01 16:49:49 +0700

    Final build

--- Assets/Script/SetCollider.cs
+++ Assets/Script/SetCollider.cs
@@ -1,26 +1,50 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class SetCollider : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject loadingScreen; // Reference to UI
+    public Slider progressBar; // Optional progress bar
+    public void LoadNextScene()
     {
-        
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            StartCoroutine(LoadSceneAsync(nextSceneIndex));
+        }
+        else
+        {
+            Debug.LogWarning("No more scenes to load! You are at the last scene.");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadSceneAsync(int sceneIndex)
     {
-        
-    }
+        loadingScreen.SetActive(true); // Show loading screen
 
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        operation.allowSceneActivation = false; // Wait until fully loaded
+
+        while (operation.progress < 0.9f)
+        {
+            if (progressBar != null)
+                progressBar.value = operation.progress; // Update UI
+
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1f); // Optional delay for smooth transition
+
+        operation.allowSceneActivation = true; // Load the scene
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            //GetComponent<ChangeScene>().nextScene();
+            LoadNextScene();
         }
     }
 }
