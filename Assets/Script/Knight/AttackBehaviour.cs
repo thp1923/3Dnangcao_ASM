a8328e9fc7472de4ad3854d34004555b44f0953e
nvmq1011 <quannvmps38853@gmail.com>
2025-03-21 11:18:39 +0700

    fix

--- Assets/Script/Knight/AttackBehaviour.cs
+++ Assets/Script/Knight/AttackBehaviour.cs
@@ -2,32 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TranBehaviour : StateMachineBehaviour
+public class AttackBehaviour : StateMachineBehaviour
 {
-    public int currentAttack;
+    public int numberAttack;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        PlayerAttackController.Instance.InputManager();
-    }
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        if (PlayerAttackController.Instance.inputRecceived)
-        {
-            animator.SetTrigger("Attack"+ currentAttack);
-            PlayerAttackController.Instance.InputManager();
-            PlayerAttackController.Instance.inputRecceived = false;
-            PlayerAttackController.Instance.isAttacking = true;
-        }
-    }
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        PlayerAttackController.Instance.inputRecceived = false;
-        animator.ResetTrigger("Attack" + currentAttack);
+        animator.ResetTrigger("Attack"+numberAttack);
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
