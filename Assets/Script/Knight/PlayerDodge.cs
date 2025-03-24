ba75d1911e707e1cdb6c580fe971731361e29be8
nvmq1011 <quannvmps38853@gmail.com>
2025-03-24 21:08:30 +0700

    dodge

--- Assets/Script/Knight/PlayerDodge.cs
+++ Assets/Script/Knight/PlayerDodge.cs
@@ -1,54 +1,44 @@
-﻿using UnityEngine;
+﻿using Invector.vCharacterController;
+using System.Collections;
+using UnityEngine;
 
 public class PlayerDodge : MonoBehaviour
 {
+
+    vThirdPersonController cp;
     public float dodgeSpeed = 10f;
-    public float dodgeDuration = 0.3f;
     public float dodgeCooldown = 1f;
 
     private bool isDodging = false;
     private bool canDodge = true;
     private Vector3 dodgeDirection;
-    private Rigidbody rb;
 
     Animator aim;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        cp = GetComponent<vThirdPersonController>();
         aim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !isDodging && canDodge)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            StartCoroutine(PerformDodge());
+            StartCoroutine(Dash());
         }
     }
 
-    private System.Collections.IEnumerator PerformDodge()
+    IEnumerator Dash()
     {
-        isDodging = true;
-        canDodge = false;
-
-        // Lấy hướng hiện tại (di chuyển hoặc nhìn)
-        dodgeDirection = transform.forward;
-
-        float timer = 0f;
-
-        while (timer < dodgeDuration)
+        float startTime = Time.time;
+        
+        while(Time.time < startTime + dodgeCooldown)
         {
-            aim.SetTrigger("Dodge");
-            rb.MovePosition(dodgeDirection * dodgeSpeed * Time.deltaTime);
-            timer += Time.deltaTime;
+            cp.MoveCharacter(cp.moveDirection * dodgeSpeed * Time.deltaTime);
+
             yield return null;
         }
-
-        isDodging = false;
-
-        // Chờ cooldown
-        yield return new WaitForSeconds(dodgeCooldown);
-        canDodge = true;
     }
+    
 }
