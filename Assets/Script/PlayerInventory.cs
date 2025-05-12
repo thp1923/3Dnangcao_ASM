425787b448b432260b627d5f3bca12cfd37e7827
ThaiiAnhh <anhphtps38912@gmail.com>
2025-05-12 19:36:37 +0700

    5/10 inventory
    bị dính vào data base nên hiện tại lỗi tè le

---
+++ Assets/Script/PlayerInventory.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public InventoryObject inventory;
+    public void OnTriggerEnter(Collider other)
+    {
+        var item = other.GetComponent<Item>();
+    if(item)
+    {
+        inventory.AddItem(item.item,1);
+        Destroy(other.gameObject);
+    }
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.RightShift))
+        {
+            inventory.Save();
+        }
+        if(Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            inventory.Load();
+        }
+    }
+}
