36b84f68bf50e3f92beaae0052244f37c8db2f64
Tran Hoang Phuc <conanthy123@gmail.com>
2025-05-22 21:32:41 +0700

    Remake Options

---
+++ Assets/Script/OptionsPanel/OptionsMenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenuController : MonoBehaviour
+{
+    #region Setup
+    public Button displayTab, audioTab, languagesTab;
+    public CanvasGroup displayGroup, audioGroup, languagesGroup;
+    public Image highlightImage;
+    public float fadeDuration = 0.2f; // Hiệu ứng fade in/out
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
