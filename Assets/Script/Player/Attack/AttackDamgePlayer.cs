66a682f80bbd5bed99a899779c69c1c9734b4dcd
nvmq1011 <quannvmps38853@gmail.com>
2025-06-12 11:50:37 +0700

    stamina

---
+++ Assets/Script/Player/Attack/AttackDamgePlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using StatsManager;
+
+public class AttackDamgePlayer : StatsAttack
+{
+    public Transform pointAttack2;
+    public Vector3 attackRange;
+    public LayerMask attackMask;
+
+    public override void Attack(int attackNumber)
+    {
+        base.Attack(attackNumber);
+        Attack1(attackNumber);
+    }
+
+    public void Attack1(int attackNum)
+    {
+        Collider[] colInfo = Physics.OverlapBox(pointAttack2.position, attackRange, Quaternion.identity, attackMask);
+        foreach (Collider enemy in colInfo)
+        {
+            enemy.GetComponent<EnemyTakeDamge>().TakeDamge(atk, stunDamge[attackNum], 0);
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(pointAttack2.position, attackRange);
+    }
+}
---
+++ Assets/Script/Player/MoveManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using Invector.vCharacterController;
+
+public class MoveManager : MonoBehaviour
+{
+    vThirdPersonController tcp;
+    Animator anim;
+    Rigidbody rb;
+    public int staminaLost = 3;
+    public float timeLostStaminaRun;
+    float _timeLostStaminaRun;
+
+    public KeyCode sprint = KeyCode.LeftShift;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tcp = GetComponent<vThirdPersonController>();
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timeLostStaminaRun -= Time.deltaTime;
+
+        if (anim.GetBool("IsSprinting") && _timeLostStaminaRun <= 0 && GetComponent<Stamina>().stamina >= staminaLost)
+        {
+            GetComponent<Stamina>().TakeStamina(staminaLost);
+            _timeLostStaminaRun = timeLostStaminaRun;
+        }
+    }
+
+    public void CheckLockMove(bool Lock)
+    {
+        tcp.lockMovement = Lock;
+        tcp.lockRotation = Lock;
+        if (Lock)
+            anim.SetFloat("InputMagnitude", -1f);
+        else
+            return;
+    }
+
+    public void CheckSleep(bool Check)
+    {
+        if (Check)
+            rb.Sleep();
+        else 
+            rb.WakeUp();
+    }
+
+    public void CheckDrag(bool Drag)
+    {
+        if (Drag)
+            rb.drag = 100f;
+        else
+            rb.drag = 0f;
+    }
+}
