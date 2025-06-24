2aff7efc5e978a120a5db89158fedc8b6b2e1240
Tran Hoang Phuc <conanthy123@gmail.com>
2025-06-24 11:22:15 +0700

    Testing

--- Assets/Script/Cutscene/CutsceneLoader.cs
+++ Assets/Script/Cutscene/CutsceneLoader.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class CutsceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public VideoPlayer videoPlayer;
+    public int nextSceneIndex = 1;
+
     void Start()
     {
-        
+        if (videoPlayer == null)
+            videoPlayer = GetComponent<VideoPlayer>();
+
+        if (videoPlayer != null)
+            videoPlayer.loopPointReached += OnVideoEnd;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnVideoEnd(VideoPlayer vp)
     {
-        
+        if (SceneLoaderr.Instance != null)
+        {
+            SceneLoaderr.Instance.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.LogError("SceneLoader instance is not available. Cannot load next scene.");
+        }
     }
 }
