f20eb87c332e790ce03ac57540d6daa54522b0b7
nvmq1011 <quannvmps38853@gmail.com>
2025-08-14 12:26:42 +0700

    fix

--- Assets/Script/Player/Shield/ShieldTransState.cs
+++ Assets/Script/Player/Shield/ShieldTransState.cs
@@ -15,16 +15,20 @@ public class ShieldTransState : StateMachineBehaviour
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.GetComponent<MoveManager>().CheckLockMove(true);
+        if (!Input.GetMouseButtonDown(1) && (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0 || Input.GetKey(KeyCode.LeftShift)))
+        {
+            animator.SetBool("hasInput", true);
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
         animator.GetComponent<MoveManager>().CheckLockMove(false);
         animator.GetComponent<MoveManager>().CheckSleep(false);
         animator.GetComponent<PlayerTakeDamge>().PlayFlame(false);
         animator.GetComponent<Stamina>().canRecover = true;
+        animator.SetBool("hasInput", false);
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
